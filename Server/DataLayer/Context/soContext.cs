22a32e62a4e12f88fcc666d94b33302a763ecc93
hermawih Hasan <hermawih@hotmail.co.id>
2023-11-16 02:48:05 +0700

    First push

---
+++ Server/DataLayer/Context/soContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.DataLayer.Context
+{
+    public class soContext : DbContext
+    {
+        public soContext(DbContextOptions<soContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Server/DataLayer/Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+    }
+}
